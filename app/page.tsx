5a4ae998a6f3bc85ee27b38f6c3e49dc03322d6c
erlandel <erlandelfino@gmail.com>
2026-05-16 14:06:41 -0400

    Agregar componente Soporte y Garantía con datos y estructura

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Brands from "@/components/brands/brands";
 import Carousel from "@/components/carousel/carousel";
 import DiscoverOurGroup from "@/components/discoverOurGroup/discoverOurGroup";
 import Promotions from "@/components/promotions/promotions";
+import SupportAndWarranty from "@/components/supportAndWarranty/supportAndWarranty";
 
 export default function Home() {
   return (
@@ -22,6 +23,11 @@ export default function Home() {
         <div>
           <Promotions />
         </div>
+
+        <div>         
+         <SupportAndWarranty />
+        </div>
+
       </div>
     </>
   );
---
+++ components/supportAndWarranty/supportAndWarranty.tsx
@@ -0,0 +1,36 @@
+import { supportItems } from "@/data/dataSupportItems";
+
+export default function SupportAndWarranty() {
+  return (
+    <section className="w-full py-16 px-4 bg-white">
+      <div className="mx-20">
+        {/* Header */}
+        <div className="text-center mb-16">
+          <h3 className="text-secondary font-semibold text-3xl mb-4">Soporte y Garantía</h3>
+          <h2 className="text-3xl md:text-5xl font-black text-dark mb-8">
+            POR QUÉ ESCOGER GRUPO FADIAR
+          </h2>
+        </div>
+
+        {/* Grid de items */}
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-8">
+          {supportItems.map((item) => {
+            const IconComponent = item.icon;
+            return (
+              <div key={item.id} className="flex gap-4 ">
+                {/* Icon + Title */}
+                  <IconComponent className="w-18 h-18 text-dark shrink-0" />
+                <div className="flex flex-col  gap-3 mb-4">
+                  <h3 className="text-3xl font-bold text-dark">{item.title}</h3>
+                <p className="text-gray-700 text-xl leading-relaxed">{item.description}</p>
+                </div>
+
+                {/* Description */}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
